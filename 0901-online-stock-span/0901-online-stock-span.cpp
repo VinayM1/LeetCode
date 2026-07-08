07f347e4f596d1926a1ce93ef98f751d5a127230
VinayM1 <vinaymeena8573@gmail.com>
2026-07-09 02:03:58 +0530

    Time: 42 ms (21.96%), Space: 92.2 MB (29.27%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,23 +1,33 @@
 class StockSpanner {
 public:
 
-    stack<pair<int,int>> st;
+    stack<pair<int,int>> st;   // {price, index}
+    int ind;
 
     StockSpanner() {
-
+        ind = -1;
     }
 
     int next(int price) {
 
-        int span = 1;
+        ind++;
 
-        while(!st.empty() && st.top().first <= price){
-            span += st.top().second;
+        while(!st.empty() && st.top().first <= price) {
             st.pop();
         }
 
-        st.push({price, span});
+        int span;
+
+        if(st.empty()) {
+            span = ind + 1;
+        }
+        else {
+            span = ind - st.top().second;
+        }
+
+        st.push({price, ind});
 
         return span;
     }
 };
+
