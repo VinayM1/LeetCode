353e1c382f291f2f2f50a19fea878ae526c43426
VinayM1 <vinaymeena8573@gmail.com>
2026-07-15 00:01:45 +0530

    Time: 0 ms (100%), Space: 12.6 MB (26.44%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -9,32 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    void Traversal(TreeNode* root){
-        if(root==NULL){
-            return ;
+
+    void Traversal(TreeNode* root)
+    {
+        if(root == NULL)
+        {
+            return;
         }
+
         queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty()){
-            int n = q.size();
-          
-            for(int i = 0;i<n;i++){
-                 TreeNode*node = q.front();
-                 q.pop();
-                 if(node->left!=NULL){
-                 q.push(node->left);
-                 }
-                 if(node->right!=NULL)
-                 q.push(node->right);
-                 swap(node->left, node->right);
-                 }
+
+        while(!q.empty())
+        {
+            TreeNode* node = q.front();
+            q.pop();
+
+            swap(node->left, node->right);
+
+            if(node->left != NULL)
+            {
+                q.push(node->left);
             }
-    
+
+            if(node->right != NULL)
+            {
+                q.push(node->right);
+            }
         }
-        
-    TreeNode* invertTree(TreeNode* root) {
+    }
+
+    TreeNode* invertTree(TreeNode* root)
+    {
         Traversal(root);
         return root;
     }
