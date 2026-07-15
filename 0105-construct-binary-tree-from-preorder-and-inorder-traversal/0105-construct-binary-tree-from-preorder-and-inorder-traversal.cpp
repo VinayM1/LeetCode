eda75ce7106b51aabe302894adf184a2a51c8777
VinayM1 <vinaymeena8573@gmail.com>
2026-07-15 21:47:55 +0530

    Time: 7 ms (33.5%), Space: 27.5 MB (33.72%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    unordered_map<int, int> inorderIndex;
+
+    TreeNode* build(vector<int>& preorder,
+                    int preStart,
+                    int preEnd,
+                    vector<int>& inorder,
+                    int inStart,
+                    int inEnd)
+    {
+        if(preStart > preEnd || inStart > inEnd)
+        {
+            return NULL;
+        }
+
+        TreeNode* root = new TreeNode(preorder[preStart]);
+
+        int index = inorderIndex[root->val];
+
+        int leftNodes = index - inStart;
+
+        root->left = build(preorder,
+                           preStart + 1,
+                           preStart + leftNodes,
+                           inorder,
+                           inStart,
+                           index - 1);
+
+        root->right = build(preorder,
+                            preStart + leftNodes + 1,
+                            preEnd,
+                            inorder,
+                            index + 1,
+                            inEnd);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+    {
+        for(int i = 0; i < inorder.size(); i++)
+        {
+            inorderIndex[inorder[i]] = i;
+        }
+
+        return build(preorder,
+                     0,
+                     preorder.size() - 1,
+                     inorder,
+                     0,
+                     inorder.size() - 1);
+    }
+};
