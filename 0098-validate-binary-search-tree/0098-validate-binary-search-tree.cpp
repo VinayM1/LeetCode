02cae01e6e75f349300484261c01882bc3413b5b
VinayM1 <vinaymeena8573@gmail.com>
2026-07-16 21:10:04 +0530

    Time: 0 ms (100%), Space: 21.8 MB (95.35%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    TreeNode* prev = NULL;
+
+    bool inorder(TreeNode* root){
+
+        if(root == NULL){
+            return true;
+        }
+
+        if(!inorder(root->left)){
+            return false;
+        }
+
+        if(prev != NULL && prev->val >= root->val){
+            return false;
+        }
+
+        prev = root;
+
+        return inorder(root->right);
+    }
+
+    bool isValidBST(TreeNode* root) {
+
+        return inorder(root);
+    }
+};
