eacaf6e5a3e1328afdc55d2862aa839696d8bde6
VinayM1 <vinaymeena8573@gmail.com>
2026-07-16 18:55:00 +0530

    Time: 0 ms (100%), Space: 34.4 MB (15.78%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* Findmax(TreeNode*root){
+        while(root->right!=NULL){
+            root = root->right;
+        }
+        return root;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL){
+            return NULL;
+        }
+        
+
+        if(root->val>key){
+            root->left = deleteNode(root->left,key);
+        }
+        else if (root->val<key){
+            root->right = deleteNode(root->right,key);
+        }
+        
+
+        
+        else{
+
+            if(root->left == NULL && root->right == NULL){
+            delete(root);
+            return NULL;
+            }
+            
+            if(root->right == NULL){
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+            if(root->left == NULL){
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }   
+            if(root->right != NULL && root->left != NULL){
+               TreeNode* predecessor = Findmax(root->left);
+                root->val = predecessor->val;
+                root->left = deleteNode(root->left, predecessor->val);
+            }
+        }
+        return root;
+    }
+};
