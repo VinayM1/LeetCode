fadd5fd93b48eddac67cc8ae79a34e6ee3642c46
VinayM1 <vinaymeena8573@gmail.com>
2026-07-27 19:30:17 +0530

    Time: 3 ms (88.85%), Space: 23.8 MB (59.39%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+
+        vector<vector<int>> ans;
+
+        for(int i = 0; i < n; i++) {
+            // If ans is empty OR no overlap
+            if(ans.empty() || arr[i][0] > ans.back()[1]) {
+                ans.push_back(arr[i]);
+            }
+            else {
+                // Merge intervals
+                ans.back()[1] = max(ans.back()[1], arr[i][1]);
+            }
+        }
+
+        return ans;
+    }
+};
