e056fa39d2bb9dfe1695599fe7c0ce03296a2776
VinayM1 <vinaymeena8573@gmail.com>
2026-08-01 17:35:00 +0530

    Time: 20 ms (46.54%), Space: 26.2 MB (36.77%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,41 +1,53 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+
         int n = grid.size();
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1){
+
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1)
             return -1;
-        }
-        vector<vector<int>> dist(n,vector<int>(n,INT_MAX)); // DIST GRID ALL INFINITY INITIALLY 
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> 
-        >pq;
-        dist[0][0] = 1; // Marking starting node
-        pq.push({1,{0,0}});// pushing in queue using (distance,{row,col})
-        int delrow [] = {-1,-1,-1,0,0,1,1,1};
-        int delcol [] = {-1,0,1,-1,1,-1,0,1};
-        while(!pq.empty()){
-            int distance  = pq.top().first;
-            int row = pq.top().second.first;
-            int col = pq.top().second.second;
-            pq.pop();
-            if(row == n-1 && col == n-1){
+
+        queue<pair<pair<int,int>,int>> q;
+
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+
+        dist[0][0] = 1;
+
+        q.push({{0,0},1});
+
+        int delRow[] = {-1,-1,-1,0,0,1,1,1};
+        int delCol[] = {-1,0,1,-1,1,-1,0,1};
+
+        while(!q.empty())
+        {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int distance = q.front().second;
+
+            q.pop();
+
+            if(row == n-1 && col == n-1)
                 return distance;
-            }
-            for(int i = 0;i<8;i++){// i<8 cause 8 directions 
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<n && grid[nrow][ncol] == 0){
-                    if(distance+1<dist[nrow][ncol]){
-                        dist[nrow][ncol] = distance+1;
-                        pq.push({dist[nrow][ncol],{nrow,ncol}});
-                    }
+
+            for(int i = 0; i < 8; i++)
+            {
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if(nrow >= 0 &&
+                   ncol >= 0 &&
+                   nrow < n &&
+                   ncol < n &&
+                   grid[nrow][ncol] == 0 &&
+                   distance + 1 < dist[nrow][ncol])
+                {
+                    dist[nrow][ncol] = distance + 1;
+
+                    q.push({{nrow,ncol}, distance + 1});
                 }
-                
-
             }
-            
-
         }
+
         return -1;
-        
     }
 };
