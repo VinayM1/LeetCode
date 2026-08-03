4d3017d7f406a190c98829105537b047cf0e97a9
VinayM1 <vinaymeena8573@gmail.com>
2026-08-03 20:29:52 +0530

    Time: 39 ms (36.58%), Space: 18.4 MB (78.92%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -41,7 +41,7 @@ public:
         Disjoint ds(n);
         int count = 0;
         for(int i = 0 ; i<n;i++){
-            for(int j = 0;j<n;j++){
+            for(int j = i+1;j<n;j++){
                 if(stones[i][0] == stones[j][0] ||
                 stones[i][1] == stones[j][1]){
                     ds.UnionBySize(i,j);
