ad31c74e193fb4513fff5c3948a886bd470fe57f
VinayM1 <vinaymeena8573@gmail.com>
2026-08-07 21:03:35 +0530

    Time: 131 ms (55.57%), Space: 110.5 MB (32.82%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,61 +1,5 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-
-        int n = nums.size();
-
-        int sum = 0;
-        for(int i = 0; i < n; i++)
-        {
-            sum += nums[i];
-        }
-
-        if(sum % 2 != 0)
-        {
-            return false;
-        }
-
-        int target = sum / 2;
-
-        vector<bool> prev(target + 1, false);
-
-        // Base Case 1
-        prev[0] = true;
-
-        // Base Case 2
-        if(nums[0] <= target)
-        {
-            prev[nums[0]] = true;
-        }
-
-        for(int index = 1; index < n; index++)
-        {
-            vector<bool> curr(target + 1, false);
-
-            curr[0] = true;
-
-            for(int t = 1; t <= target; t++)
-            {
-                bool notTake = prev[t];
-
-                bool take = false;
-
-                if(nums[index] <= t)
-                {
-                    take = prev[t - nums[index]];
-                }
-
-                curr[t] = take || notTake;
-            }
-
-            prev = curr;
-        }
-
-        return prev[target];
-    }
-};
-/*class Solution {
-public:
     bool Solve(int index,int target, vector<int>& nums,vector<vector<int>>& dp){
         
         if(target == 0)
@@ -89,4 +33,4 @@ public:
         vector<vector<int>>dp(n,vector<int>(target+1,-1));
         return Solve(nums.size()-1,target,nums,dp);
     }
-};*/
+};
