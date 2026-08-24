72fac68b7e10cdf403b76ac4143006f9b1c05bf1
VinayM1 <vinaymeena8573@gmail.com>
2026-08-24 16:57:50 +0530

    Time: 210 ms (7.45%), Space: 22.9 MB (7.71%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     int Solve(vector<int>& nums,int index,vector<int>& dp){
         int n = nums.size();
-        int minsum = 1000000;
+        int minsum = INT_MAX;
         if(index>=n-1){
             return 0;
         }
         if(dp[index] != -1){
             return dp[index];
         }
-        for(int i = 1 ; i<=nums[index] ; i++){
-            minsum = min(minsum,1 + Solve(nums,index+i,dp));
+        for(int i = 1; i <= nums[index]; i++) {
+            int steps = Solve(nums, index+i, dp);
+
+            if(steps != INT_MAX) {
+                minsum = min(minsum, 1 + steps);
+            }
         }
         return dp[index] = minsum;
     }
